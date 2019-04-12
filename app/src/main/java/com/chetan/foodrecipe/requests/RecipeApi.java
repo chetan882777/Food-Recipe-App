f36d4108b405ab173406abd717c365d9b459c8e6
chetan882777 <chetan882777@gmail.com>
2019-04-12 13:16:52 -0700

    Retrofit interface methods

--- app/src/main/java/com/chetan/foodrecipe/requests/RecipeApi.java
+++ app/src/main/java/com/chetan/foodrecipe/requests/RecipeApi.java
@@ -1,4 +1,28 @@
 package com.chetan.foodrecipe.requests;
 
+import com.chetan.foodrecipe.models.Recipe;
+import com.chetan.foodrecipe.requests.responses.RecipeResponse;
+import com.chetan.foodrecipe.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+
+    //search
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+
+    // Get recipe request
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
 }
