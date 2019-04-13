1ae316cec370b42fed68c7cb4dd9602236331bf3
chetan882777 <chetan882777@gmail.com>
2019-04-13 09:01:01 -0700

    setting up recycler viw adapter

---
+++ app/src/main/java/com/chetan/foodrecipe/adapter/OnRecipeListener.java
@@ -0,0 +1,8 @@
+package com.chetan.foodrecipe.adapter;
+
+public interface OnRecipeListener {
+
+    void onRecipeClick(int position);
+
+    void onCategoryClick(String category);
+}
---
+++ app/src/main/java/com/chetan/foodrecipe/adapter/RecipeRecyclerAdapter.java
@@ -0,0 +1,49 @@
+package com.chetan.foodrecipe.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chetan.foodrecipe.R;
+import com.chetan.foodrecipe.models.Recipe;
+
+import java.util.List;
+
+public class RecipeRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Recipe> mRecipes;
+    private OnRecipeListener onRecipeListener;
+
+    public RecipeRecyclerAdapter(OnRecipeListener onRecipeListener) {
+        this.onRecipeListener = onRecipeListener;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_recipe_list_item,
+                viewGroup , false);
+
+        return new RecipeViewHolder(view , onRecipeListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        ((RecipeViewHolder)viewHolder).title.setText(mRecipes.get(i).getTitle());
+        ((RecipeViewHolder)viewHolder).publisher.setText(mRecipes.get(i).getPublisher());
+        ((RecipeViewHolder)viewHolder).socialScore.setText(
+                String.valueOf(Math.round(mRecipes.get(i).getSocial_rank())));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+
+    public void setRecipes(List<Recipe> recipes){
+        mRecipes = recipes;
+        notifyDataSetChanged();
+    }
+}
