c5a213f2b7e05f5a6999378fccbbf78420087983
chetan882777 <chetan882777@gmail.com>
2019-04-21 12:07:21 -0700

    Added error screen

--- app/src/main/java/com/chetan/foodrecipe/RecipeActivity.java
+++ app/src/main/java/com/chetan/foodrecipe/RecipeActivity.java
@@ -71,9 +71,39 @@ public class RecipeActivity extends BaseActivity {
             public void onChanged(@Nullable Boolean aBoolean) {
                 if(aBoolean && mRecipeViewModel.getDidiRetrieveRecipe()){
                     Log.d(TAG, "onChanged: network time out");
+                    displayErrorScreen("Error retrieving data. Check network connection.");
                 }
             }
         });
+    }
+
+
+    private void displayErrorScreen(String errorMessage){
+        mRecipeTitle.setText("Error retrieveing recipe...");
+        mRecipeRank.setText("");
+        TextView textView = new TextView(this);
+        if(!errorMessage.equals("")){
+            textView.setText(errorMessage);
+        }
+        else{
+            textView.setText("Error");
+        }
+        textView.setTextSize(15);
+        textView.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        mRecipeIngredientsContainer.addView(textView);
+
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background);
+
+        Glide.with(this)
+                .setDefaultRequestOptions(requestOptions)
+                .load(R.drawable.ic_launcher_background)
+                .into(mRecipeImage);
+
+        showParent();
+        showProgressBar(false);
     }
 
 
